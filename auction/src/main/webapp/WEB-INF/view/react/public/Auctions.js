118210dbdcd21f96fb976115d34f2e398a71b19d
tom <tom@192.168.1.109>
2021-01-22 00:29:37 +0200

    Display all auctions in homepage

--- auction/src/main/webapp/WEB-INF/view/react/public/Auctions.js
+++ auction/src/main/webapp/WEB-INF/view/react/public/Auctions.js
@@ -1,12 +1,70 @@
 const { Component } = React;
 const { render } = ReactDOM;
+const axios = axios;
 
-const Auctions = () => {
-    return (
-		<div>
-            <h1>Section for displaying all auctions</h1>
-        </div>
-    );
+const AUCTIONS_API = 'http://localhost:8080/api'
+const GET_AUCTIONS_API_URL = `${AUCTIONS_API}/auctions`
+
+class ListAuctionsComponent extends Component {
+	
+    constructor(props) {
+        super(props)
+        this.state = {
+            auctions: [],
+            message: null
+        }
+        this.refreshAuctions = this.refreshAuctions.bind(this)
+    }
+
+    componentDidMount() {
+        this.refreshAuctions();
+    }
+
+    retrieveAllAuctions() {
+        return axios.get(`${GET_AUCTIONS_API_URL}`);
+    }
+
+    refreshAuctions() {
+        this.retrieveAllAuctions()
+            .then(
+                response => {
+                    this.setState({ auctions: response.data });         
+				}
+            )
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h3>All Auctions</h3>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+								<th>Description</th>
+								<th>Time_Limit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+							{
+								this.state.auctions.map(
+									auction =>
+									<tr key={auction.id}>
+		                                <td>{auction.id}</td>
+										<td>{auction.name}</td>
+										<td>{auction.description}</td>
+										<td>{auction.timeLimit}</td>		                                
+		                            </tr> 
+								)
+							}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(<Auctions />, document.getElementById('all-auctions'));
+ReactDOM.render(<ListAuctionsComponent />, document.getElementById('all-auctions'));
